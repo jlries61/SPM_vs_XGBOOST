67156f79e962de0631cec3760615cb34437fe907
John L. Ries <jries@salford-systems.com>
2020-01-23 21:22:22 -0800

    Add Dexter and Springleaf to create_data_samples4.py

--- Scripts/create_data_samples4.py
+++ Scripts/create_data_samples4.py
@@ -14,7 +14,7 @@ rseed=37
 data_folder ="../Data/Classification/"
 dsl = pd.read_excel("../Datasets4.xlsx")
 
-datasets_nums = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17]
+datasets_nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 for i_ds in datasets_nums:
     name = dsl['Name'][i_ds]
